cd4e5c71f23aa25ae4b7cbc3ad2afc78c7b1aaa7
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-03-12 15:22:18 +0000

    lower than or higher than inversion fix and when an assignment statement is used without a symbol add brackets around it and invert it

--- code.js
+++ code.js
@@ -1,24 +1,43 @@
 // retrieve outer conditions in an if statement so that only they would need to be changed
 // invert outer conditions accordingly
 
+function dealWithStandaloneStatements(logicalOperatorFound, isMultipleStatements, needBrackets, conditionIndexes, indexOfNewStatement, i) {
+    if (!logicalOperatorFound) {
+        conditionIndexes.push({ start: indexOfNewStatement, end: i - 1});
+    }
+    if (isMultipleStatements && needBrackets) {
+        conditionIndexes.push({ brackets: true, start: indexOfNewStatement, end: i });
+    }
+}
+
 function identifyConditions(tokens, ifStatementlocationsInTokens, firstIfStatementCloseBracketIndex) {
     const conditionIndexes = [];
-    let signFound = false;
+    let logicalOperatorFound = false;
     let indexOfNewStatement = ifStatementlocationsInTokens + 2;
+    let isMultipleStatements = false;
+    let needBrackets = true;
     for (let i = ifStatementlocationsInTokens + 2; i < firstIfStatementCloseBracketIndex; i += 1) {
         if (tokens[i] === '&' || tokens[i] === '|') {
             if (tokens[i + 1] === '&' || tokens[i + 1] === '|') {
-                if (!signFound) {
-                    conditionIndexes.push({ start: indexOfNewStatement, end: i - 1});
-                }
+                dealWithStandaloneStatements(logicalOperatorFound, isMultipleStatements, needBrackets, conditionIndexes, indexOfNewStatement, i);
                 conditionIndexes.push({ start: i});
                 indexOfNewStatement = i + 2;
-                signFound = false;
+                logicalOperatorFound = false;
+                isMultipleStatements = false;
+                needBrackets = true;
                 i += 1;
             }
-        } else if (tokens[i] === '=' || tokens[i] === '<' || tokens[i] === '>' || tokens[i] === '!') {
+        } else if (tokens[i] === '<' || tokens[i] === '>') {
+            logicalOperatorFound = true;
+            if (tokens[i + 1] === '=') {
+                conditionIndexes.push({ start: i, hasFollowupEquals: true });
+                i += 1;
+            } else {
+                conditionIndexes.push({ start: i });
+            }
+        } else if (tokens[i] === '=' || tokens[i] === '!') {
             conditionIndexes.push({ start: i });
-            signFound = true;
+            logicalOperatorFound = true;
             if (tokens[i + 1] === '=') {
                 if (tokens[i + 2] === '=') {
                     i += 2;
@@ -26,50 +45,74 @@ function identifyConditions(tokens, ifStatementlocationsInTokens, firstIfStateme
                     i += 1;
                 }
             }
+        } else if (tokens[i] === '-' || tokens[i] === '+' || tokens[i] === '/' || tokens[i] === '*') {
+            isMultipleStatements = true;
+        } else if (tokens[i] === '(') {
+            needBrackets = false;
         }
     }
-    if (!signFound) {
-        conditionIndexes.push({ start: indexOfNewStatement, end: firstIfStatementCloseBracketIndex - 1 });
-    }
+    dealWithStandaloneStatements(logicalOperatorFound, isMultipleStatements, needBrackets, conditionIndexes, indexOfNewStatement, firstIfStatementCloseBracketIndex);
     return conditionIndexes;
 }
 
 function invertIfStatements(tokens, conditionIndexes) {
     let newElementsDelta = 0;
-    conditionIndexes.forEach(({ start }) => {
+    conditionIndexes.forEach(({ start, end, brackets, hasFollowupEquals }) => {
         const arrayIndex = start + newElementsDelta;
-        switch (tokens[arrayIndex]) {
-            case '=':
-                tokens[arrayIndex] = '!';
-                break;
-            case '<':
-                tokens[arrayIndex] = '>';
-                break;
-            case '>':
-                tokens[arrayIndex] = '<';
-                break;
-            case '&':
-                tokens[arrayIndex] = '|';
-                tokens[arrayIndex + 1] = '|';
-                break;
-            case '|':
-                tokens[arrayIndex] = '&';
-                tokens[arrayIndex + 1] = '&';
-                break;
-            case '!':
-                if (tokens[arrayIndex + 1] === '=') {
-                    tokens[arrayIndex] = '=';
-                } else {
-                    tokens.splice(arrayIndex, 1);
-                    newElementsDelta -= 1;
+        if (brackets) {
+            const endArrayIndex = end + newElementsDelta;
+            tokens.splice(arrayIndex, 0, '(');
+            tokens.splice(endArrayIndex + 1, 0, ')');
+            newElementsDelta += 2;
+        } else {
+            switch (tokens[arrayIndex]) {
+                case '=':
+                    tokens[arrayIndex] = '!';
+                    break;
+                case '<':
+                    tokens[arrayIndex] = '>';
+                    if (hasFollowupEquals) {
+                        tokens.splice(arrayIndex + 1, 1);
+                        newElementsDelta -= 1;
+                    } else {
+                        tokens.splice(arrayIndex + 1, 0, '=');
+                        newElementsDelta += 1;
+                    }
+                    break;
+                case '>':
+                    tokens[arrayIndex] = '<';
+                    if (hasFollowupEquals) {
+                        tokens.splice(arrayIndex + 1, 1);
+                        newElementsDelta -= 1;
+                    } else {
+                        tokens.splice(arrayIndex + 1, 0, '=');
+                        newElementsDelta += 1;
+                    }
+                    break;
+                case '&':
+                    tokens[arrayIndex] = '|';
+                    tokens[arrayIndex + 1] = '|';
+                    break;
+                case '|':
+                    tokens[arrayIndex] = '&';
+                    tokens[arrayIndex + 1] = '&';
+                    break;
+                case '!':
+                    if (tokens[arrayIndex + 1] === '=') {
+                        tokens[arrayIndex] = '=';
+                    } else {
+                        tokens.splice(arrayIndex, 1);
+                        newElementsDelta -= 1;
+                    }
+                    break;
+                default: {
+                    tokens.splice(arrayIndex, 0, '!');
+                    newElementsDelta += 1;
                 }
-                break;
-            default: {
-                tokens.splice(arrayIndex, 0, '!');
-                newElementsDelta += 1;
             }
         }
-    })
+        
+    });
 }
 
 function getIndexOfLastBracketOfIfStatement(tokens, firstIndex, openBrackets = 0) {
@@ -105,8 +148,8 @@ function runInvert(functionString) {
     const tokens = tokenize(functionString);
     const ifStatementIndexes = retrieveIfIndexes(tokens);
     ifStatementIndexes.forEach(({ start, end }) => {
-        const conditions = identifyConditions(tokens, start, end);
-        invertIfStatements(tokens, conditions);
+        const conditionIndexes = identifyConditions(tokens, start, end);
+        invertIfStatements(tokens, conditionIndexes);
     });
     return tokens.join('');
 }
@@ -125,7 +168,7 @@ function test(input, expectedResult) {
 function runTests() {
     test(
         'if ((hello) === (2) && start || number < 2 && hello && end) { console.log(2) }',
-        'if((hello)!==(2)||!start&&number>2||!hello||!end){console.log(2)}',
+        'if((hello)!==(2)||!start&&number>=2||!hello||!end){console.log(2)}',
     );
 
     test(
@@ -146,6 +189,36 @@ function runTests() {
     test(
         'if (dog && cat || mouse) { console.log(2) }',
         'if(!dog||!cat&&!mouse){console.log(2)}',
+    );
+
+    test(
+        'if (dog - cat || mouse) { console.log(2) }',
+        'if(!(dog-cat)&&!mouse){console.log(2)}',
+    );
+
+    test(
+        'if (mouse || dog - cat) { console.log(2) }',
+        'if(!mouse&&!(dog-cat)){console.log(2)}',
+    );
+
+    test(
+        'if (mouse < cat) { console.log(2) }',
+        'if(mouse>=cat){console.log(2)}',
+    );
+
+    test(
+        'if (mouse <= cat) { console.log(2) }',
+        'if(mouse>cat){console.log(2)}',
+    );
+
+    test(
+        'if (mouse <= cat || hello) { console.log(2) }',
+        'if(mouse>cat&&!hello){console.log(2)}',
+    );
+
+    test(
+        'if (hello || mouse <= cat) { console.log(2) }',
+        'if(!hello&&mouse>cat){console.log(2)}',
     );
 }
 
