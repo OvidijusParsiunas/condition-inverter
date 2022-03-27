e2f18c6033c048abc35516516d01332464817f4d
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-03-27 23:38:17 +0100

    added more eslint rules

---
+++ extension/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: 'tsconfig.json',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/naming-convention': 'warn',
+    '@typescript-eslint/semi': 'warn',
+    curly: 'warn',
+    eqeqeq: 'warn',
+    'no-throw-literal': 'warn',
+    semi: 'off',
+    '@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
+    'max-len': ['error', { code: 123 }],
+    'prefer-template': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+  },
+  ignorePatterns: ['out', 'dist', '**/*.d.ts'],
+};
