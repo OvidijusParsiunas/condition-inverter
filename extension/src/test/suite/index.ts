ec17dcf386393114c92cc2753ee35f5e06944277
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-03-31 22:28:29 +0100

    added istanbul code coverage

--- extension/src/test/suite/index.ts
+++ extension/src/test/suite/index.ts
@@ -2,7 +2,36 @@ import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
-export function run(): Promise<void> {
+interface NYCWithBasicMethods {
+  writeCoverageFile: () => void;
+  report: () => void;
+}
+
+// create an nyc instance, config here is the same as your package.json
+function setupCoverage(): NYCWithBasicMethods {
+  const NYC = require('nyc');
+  const nyc = new NYC({
+    cwd: path.join(__dirname, '..', '..', '..'),
+    exclude: ['**/test/**', '.vscode-test/**'],
+    reporter: ['text', 'html'],
+    all: true,
+    instrument: true,
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true,
+    branches: 85,
+    lines: 95,
+    statements: 95,
+    functions: 100,
+  });
+  nyc.reset();
+  nyc.wrap();
+  return nyc;
+}
+
+export async function run(): Promise<void> {
+  const nyc = process.env.COVERAGE ? setupCoverage() : null;
+
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -10,29 +39,27 @@ export function run(): Promise<void> {
   });
 
   const testsRoot = path.resolve(__dirname, '..');
+  const files = glob.sync('**/**.test.js', { cwd: testsRoot });
+  // Add files to the test suite
+  files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
-  return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run((failures) => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        console.error(err);
-        e(err);
-      }
+  try {
+    await new Promise<void>((resolve, reject) => {
+      // Run the mocha test
+      mocha.run((failures) => {
+        if (failures > 0) {
+          reject(new Error(`${failures} tests failed.`));
+        } else {
+          resolve();
+        }
+      });
     });
-  });
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (nyc) {
+      nyc.writeCoverageFile();
+      nyc.report();
+    }
+  }
 }
