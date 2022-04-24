e10c6b837c75988de5f9286961ee169f66d77436
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-04-24 22:09:03 +0100

    style refactoring

--- website/src/components/error/ErrorSnackbar.tsx
+++ website/src/components/error/ErrorSnackbar.tsx
@@ -11,23 +11,15 @@ interface Props {
 export default function ErrorSnackbar(props: Props) {
   const { display, closeCallback } = props;
 
-  // const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
-  const handleClose = () => {
-    closeCallback();
-    // if (reason === 'clickaway') {
-    //   return;
-    // }
-  };
-
   return (
     <div>
       <Snackbar
         open={display}
-        onClose={handleClose}
+        onClose={closeCallback}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         message={UNEXPECTED_ERROR_MESSAGE_PREFIX}
       >
-        <Alert id="error-alert" onClose={handleClose} icon={false}>
+        <Alert id="error-alert" onClose={closeCallback} icon={false}>
           <div id="error-alert-text-prefix">{UNEXPECTED_ERROR_MESSAGE_PREFIX}</div>
           <a id="error-alert-url-text" href={UNEXPECTED_ERROR_MESSAGE_LINK} target="_blank" rel="noreferrer">
             {UNEXPECTED_ERROR_MESSAGE_LINK}
