50c166dc59dd1ea1a92e1d4173c71fc09058ca31
OvidijusParsiunas <oparsiunas@googlemail.com>
2022-05-07 18:43:28 +0000

    refactored website title and deploying it to github-pages

--- website/src/components/header/title/Title.tsx
+++ website/src/components/header/title/Title.tsx
@@ -1,5 +1,12 @@
 import './title.css';
 
 export default function Title() {
-  return <div id="title">If (Inverter)</div>;
+  // WORK: refactor
+  return (
+    <div id="title">
+      <div style={{ float: 'left', fontSize: 33 }}>If (</div>
+      <div style={{ float: 'left' }}>Inverter</div>
+      <div style={{ marginTop: 0, fontSize: 33 }}>)</div>
+    </div>
+  );
 }
