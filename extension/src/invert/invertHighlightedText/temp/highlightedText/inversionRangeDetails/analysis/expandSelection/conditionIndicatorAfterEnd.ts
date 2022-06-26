8361b5a3ea91c7574709ea0ef3f501b12e058b78
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-06-26 21:52:50 +0100

    refactoring work

--- extension/src/invert/invertHighlightedText/temp/highlightedText/inversionRangeDetails/analysis/expandSelection/conditionIndicatorAfterEnd.ts
+++ extension/src/invert/invertHighlightedText/temp/highlightedText/inversionRangeDetails/analysis/expandSelection/conditionIndicatorAfterEnd.ts
@@ -14,7 +14,7 @@ import { LineTokenTraversalUtils } from '../shared/lineTokenTraversalUtils';
 import { Token, Tokens } from 'shared/inverter/src/shared/types/tokens';
 import { Position } from '../../../../../../../shared/types/position';
 import { RangeCreator } from '../../../../../../shared/rangeCreator';
-import { TextEditor } from 'vscode';
+import { Range, TextEditor } from 'vscode';
 
 export class ConditionIndicatorAfterEnd {
   private static generateEndOperatorPadding(conditionIndicatorPresent: boolean, conditionIndicatorToken: Token): string {
@@ -68,20 +68,61 @@ export class ConditionIndicatorAfterEnd {
     return true;
   }
 
-  private static isEndOnOrAfterConditionIndicator(editor: TextEditor, line: number, endChar?: number): boolean {
+  private static getNonSpaceCharacterLeftAndUpwards(editor: TextEditor, line: number, endChar?: number): Token | null {
     endChar ??= editor.document.lineAt(line).range.end.character;
     const lineTokens = LineTokenTraversalUtils.getLineTokensBeforeCharNumber(editor, line, endChar);
     for (let i = lineTokens.length - 1; i >= 0; i -= 1) {
       if (!SPACE_JSON[lineTokens[i] as string]) {
+        return lineTokens[i];
+      }
+    }
+    if (line - 1 < 0) return null;
+    return ConditionIndicatorAfterEnd.getNonSpaceCharacterLeftAndUpwards(editor, line - 1);
+  }
+
+  private static isAfterStatementToken(editor: TextEditor, line: number, lineTokens: Tokens): boolean {
+    const siblingTokenResult = ConditionIndicatorAfterEnd.getNonSpaceCharacterLeftAndUpwards(editor, line, lineTokens.join('').length);
+    // WORK - what about the else word in else if
+    return STATEMENT_JSON[siblingTokenResult as keyof typeof STATEMENT_JSON];
+  }
+
+  private static isConditionIndicator2(editor: TextEditor, line: number, lineTokens: Tokens): boolean {
+    const isAfterStatementToken = ConditionIndicatorAfterEnd.isAfterStatementToken(editor, line, lineTokens);
+    if (isAfterStatementToken) return true;
+    return ConditionIndicatorValidator.isTokenIndexPartOfConditionIndicator(lineTokens, lineTokens.length - 1, false);
+  }
+
+  private static isEndAfterConditionIndicator(editor: TextEditor, line: number, endChar?: number): boolean {
+    endChar ??= editor.document.lineAt(line).range.end.character;
+    const lineTokens = LineTokenTraversalUtils.getLineTokensBeforeCharNumber(editor, line, endChar);
+    for (let i = lineTokens.length - 1; i >= 0; i -= 1) {
+      if (!SPACE_JSON[lineTokens[i] as string]) {
+        if (lineTokens[i] === '(') {
+          // WORK - should not be a problem for statements with no brackets (check)
+          // it is ok that this does not work for for loops for (let i = 0;| dog > cat
+          return ConditionIndicatorAfterEnd.isConditionIndicator2(editor, line, lineTokens.slice(0, i - 1));
+        }
         return ConditionIndicatorValidator.isTokenIndexPartOfConditionIndicator(lineTokens, i, false);
       }
     }
     if (line - 1 < 0) return false;
-    return ConditionIndicatorAfterEnd.isEndOnOrAfterConditionIndicator(editor, line - 1);
+    return ConditionIndicatorAfterEnd.isEndAfterConditionIndicator(editor, line - 1);
   }
 
-  public static getEndPositionDetails(editor: TextEditor, highlightEnd: Position, startPositionDetails: StartPositionDetails): EndPositionDetails {
-    if (!ConditionIndicatorAfterEnd.isEndOnOrAfterConditionIndicator(editor, highlightEnd.line, highlightEnd.character)) {
+  private static isEndOnOrAfterConditionIndicator(editor: TextEditor, highlightEnd: Position): boolean {
+    const charAfterEnd = editor.document.getText(
+      RangeCreator.create(highlightEnd, { line: highlightEnd.line, character: highlightEnd.character + 1 }),
+    );
+    if (Object.keys(SPACE_JSON).indexOf(charAfterEnd) === -1 && charAfterEnd !== '') {
+      const lineTokens = LineTokenTraversalUtils.getLineTokensBeforeCharNumber(editor, highlightEnd.line, highlightEnd.character);
+      return ConditionIndicatorAfterEnd.isConditionIndicator2(editor, highlightEnd.line, lineTokens);
+    }
+    return ConditionIndicatorAfterEnd.isEndAfterConditionIndicator(editor, highlightEnd.line, highlightEnd.character);
+  }
+
+  public static getEndPositionDetails(editor: TextEditor, fullWordRange: Range, startPositionDetails: StartPositionDetails): EndPositionDetails {
+    const highlightEnd = fullWordRange.end;
+    if (!ConditionIndicatorAfterEnd.isEndOnOrAfterConditionIndicator(editor, highlightEnd)) {
       const endPositionDetails = ConditionIndicatorAfterEnd.searchRightAndDownwards(
         editor,
         ConditionIndicatorAfterEnd.isConditionIndicatorPresent(editor, highlightEnd, startPositionDetails),
