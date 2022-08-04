e41cbd83304a17d27c7296802c3b2cf6e65d4ba5
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2022-08-04 23:36:08 +0100

    updated title

--- website/src/components/header/title/Title.tsx
+++ website/src/components/header/title/Title.tsx
@@ -1,12 +1,5 @@
 import './title.css';
 
 export default function Title() {
-  return (
-    <div id="title">
-      <div className="individual-title-content">Condition Inverter</div>
-      <div id="title-logo" className="individual-title-content">
-        &#60;&#62;
-      </div>
-    </div>
-  );
+  return <div id="title">Condition Inverter</div>;
 }
